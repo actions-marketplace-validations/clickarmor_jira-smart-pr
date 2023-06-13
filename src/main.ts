c4ccd784ff68a9fbe1ff3d381563cb1fd5f2c8cc
Joshua Vinters <jvinters@clickarmor.ca>
2023-06-13 13:13:50 -0400

    feat: refactored to allow a custom regex

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,74 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const token: string = core.getInput('repo-token');
-    const jira: string = core.getInput('jira-url');
-    const projectPrefix: string = core.getInput('project-prefix');
-    const octokit = github.getOctokit(token);
+    const INPUTS_GITHUB_TOKEN: string = core.getInput('token')
+    const INPUTS_ISSUE_PREFIX: string = core.getInput('issue-prefix')
+    const INPUTS_JIRA_URL: string = core.getInput('jira-url')
+    const INPUTS_ISSUE_REGEXP: string = core.getInput('issue-regexp')
+    const INPUTS_GITHUB_OCTOKIT = github.getOctokit(INPUTS_GITHUB_TOKEN)
 
     if (!github.context.payload.pull_request) {
-      throw Error('No pull request payload');
+      throw Error('No pull request payload')
     }
 
     if (!github.context.payload.repository) {
-      throw Error('Error loading repo payload');
+      throw Error('Error loading repo payload')
     }
 
     if (!github.context.payload.repository.owner.login) {
-      throw Error('Error loading owner payload');
+      throw Error('Error loading owner payload')
     }
 
-    const branchName: string = github.context.payload.pull_request.head.ref;
-    const issue_number: number = github.context.payload.pull_request.number;
-    const owner: string = github.context.payload.repository.owner.login;
-    const repo: string = github.context.payload.repository.name;
-    const regex: RegExp = new RegExp(`${projectPrefix}-[0-9]+`, 'gm');
-    const ticketMatches: string[] = branchName.match(regex) || [];
+    const DEFAULT_ISSUE_REGEXP = new RegExp(`${INPUTS_ISSUE_PREFIX}-[0-9]+`, 'gm')
+
+    const GITHUB_BRANCH_NAME: string = github.context.payload.pull_request.head.ref
+    const GITHUB_PR_NUMBER: number = github.context.payload.pull_request.number
+    const GITHUB_REPO_OWNER_NAME: string = github.context.payload.repository.owner.login
+    const GITHUB_REPO_NAME: string = github.context.payload.repository.name
+
+    const shouldUseCustomRegexp = !!INPUTS_ISSUE_REGEXP
+
+    const regex: RegExp = shouldUseCustomRegexp
+      ? new RegExp(INPUTS_ISSUE_REGEXP, 'gm')
+      : new RegExp(DEFAULT_ISSUE_REGEXP, 'gm')
+
+    const ticketMatches: string[] = GITHUB_BRANCH_NAME.match(regex) || []
 
     if (ticketMatches.length) {
-      const ticketId = ticketMatches.reverse()[0]; // get last matching jira ticket
-      const body: string = `Jira Ticket: [${jira}/jira/browse/${ticketId}](${jira}/jira/browse/${ticketId})`;
+      const ticketId = ticketMatches.reverse()[0] // get last matching jira ticket
+      const body = `Jira Ticket: [${INPUTS_JIRA_URL}/${ticketId}](${INPUTS_JIRA_URL}/${ticketId})`
 
-      const comments = await octokit.issues.listComments({
-        issue_number,
-        owner,
-        repo
-      });
+      const comments = await INPUTS_GITHUB_OCTOKIT.issues.listComments({
+        issue_number: GITHUB_PR_NUMBER,
+        owner: GITHUB_REPO_OWNER_NAME,
+        repo: GITHUB_REPO_NAME
+      })
 
-      const foundComment: boolean = !!comments.data.find(it => it.body === body);
+      const foundComment = !!comments.data.find(it => it.body === body)
 
       if (!foundComment) {
-        await octokit.issues.createComment({
+        await INPUTS_GITHUB_OCTOKIT.issues.createComment({
           body,
-          issue_number,
-          repo,
-          owner
-        });
+          issue_number: GITHUB_PR_NUMBER,
+          repo: GITHUB_REPO_NAME,
+          owner: GITHUB_REPO_OWNER_NAME
+        })
       } else {
-        console.info('Jira ticket already assigned');
+        console.info('Jira ticket already assigned')
       }
     } else {
-      console.info('No jira ticket found in branch');
+      console.info('No jira ticket found in branch')
     }
-  } catch (error) {
-    core.setFailed(error.message);
+  } catch (error: unknown) {
+    let errorMessage = 'Failed to run action.'
+    if (error instanceof Error) {
+      errorMessage = error.message
+    }
+
+    core.setFailed(errorMessage)
   }
 }
 
-run();
+run()
